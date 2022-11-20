a9ca3e36dd62458b9de997073955b2277c662d17
zvakilov29 <zakariyyafv@code.edu.az>
2022-11-20 21:04:24 +0300

    feature: added code for abstract class Weapon

---
+++ AbstractionPolymorphism/AbstractionPolymorphism/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AbstractionPolymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sword sword1 = new Sword();
+            sword1.Attack();
+
+            Console.WriteLine();
+            MagicStaff staff1 = new MagicStaff();
+            staff1.Attack();
+            staff1.Charge();
+            staff1.Attack();
+
+            Console.WriteLine();
+            Pistol pistol1 = new Pistol();
+            pistol1.Attack();
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ AbstractionPolymorphism/AbstractionPolymorphism/Weapon.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractionPolymorphism
+{
+    public abstract class Weapon
+    {
+        public string Model { get; set; }
+        public int Damage { get; set; }
+        public int Range { get; set; }
+        public abstract void Attack();
+    }
+
+    #region WeaponClasses
+    public abstract class MeleeWeapon : Weapon
+    {
+        public int Durability { get; set; }
+    }
+    public abstract class MagicWeapon : Weapon
+    {
+        public int MagicPoints { get; set; }
+        public abstract void Charge();
+    }
+    public abstract class RangeWeapon : Weapon
+    {
+        public int CartridgeCapacity { get; set; }
+        public int CartridgeBalance { get; set; }
+        public abstract void Reload();
+    }
+    #endregion
+
+    #region ActualWeapons
+    class Sword : MeleeWeapon
+    {
+        public override void Attack()
+        {
+            if (Durability > 0)
+            {
+                Console.WriteLine("Enemy's body is hit by the sword");
+                Durability--;
+            }
+            else
+                Console.WriteLine("Warning! The sword is broken and can't be used!");
+        }
+    }
+    class MagicStaff : MagicWeapon
+    {
+        public override void Charge()
+        {
+            Console.WriteLine("The magic staff is charged with magic");
+            MagicPoints++;
+        }
+
+        public override void Attack()
+        {
+            if (MagicPoints > 0)
+            {
+                Console.WriteLine("Enemy is hit by the magic spell");
+                MagicPoints--;
+            }
+            else
+                Console.WriteLine("Warning! The staff isn't charged with magic! No spell is produced");
+        }
+    }
+    class Pistol : RangeWeapon
+    {
+        public override void Reload()
+        {
+            Console.WriteLine("Pistol is reloaded");
+            CartridgeBalance = CartridgeCapacity;
+        }
+        public override void Attack()
+        {
+            if (CartridgeBalance > 0)
+            {
+                Console.WriteLine("Enemy is shot by the pistol");
+                CartridgeBalance--;
+            }
+            else
+                Console.WriteLine("Warning! The pistol has no bullets, the cartridge is empty!");
+        }
+    }
+    #endregion
+}
